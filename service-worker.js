6385577b36d5ea710ef66a83fc8042db61a0c25a
Pantonyeung <107616897+Pantonyeung@users.noreply.github.com>
2026-07-18 22:20:31 +0800

    chore(pwa): bump page-folder preview cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='morefun-smt-page-folders-preview-v1';
+const CACHE='morefun-smt-page-folders-preview-v2';
 const SHELL=['./','./index.html','./app-shell.css','./app-loader.js','./manifest.webmanifest'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));});
