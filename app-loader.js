c5c4149314cecbeb39a5868c036b00c2194f4374
Pantonyeung <107616897+Pantonyeung@users.noreply.github.com>
2026-07-23 23:46:49 +0800

    chore(shell): publish core rewrite 10

--- app-loader.js
+++ app-loader.js
@@ -14,7 +14,7 @@ const routes = {
   more: 'pages/more/index.html'
 };
 
-const BUILD = 'smt-t2s-1280x800-core-rewrite.9';
+const BUILD = 'smt-t2s-1280x800-core-rewrite.10';
 let current = '';
 let childReady = false;
 let loadSeq = 0;
